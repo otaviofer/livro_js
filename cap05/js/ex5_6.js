dfd22c94513f4de80b68744547b13317e13d5da4
Otavio Fernandes da Silva dos Santos <otaviofernandes113@gmail.com>
2026-02-12 19:56:40 -0300

    refactor: otimização no cód ex5_6.js

--- cap05/js/ex5_6.js
+++ cap05/js/ex5_6.js
@@ -5,13 +5,14 @@ frm.addEventListener("submit", (e) => {
     e.preventDefault();
 
     const num = Number(frm.inNumero.value);
-    let numDivisores = 0;   // declara e inicializa contador
-    for(let i=1; i<=num; i++){  // percorre todos os possiveis divisores de num
-        if(num%i==0){   // verifica se i (1,2,3...) é divisor de num
-            numDivisores++; // se é, incrementa contador
+    let temDivisor = 0;   // declara e inicializa varoável tipo flag
+    for(let i=2; i<=num/2; i++){  // percorre todos os possiveis divisores de num
+        if(num%i==0){   // se tem um divisor
+            temDivisor = 1; // muda o flag
+            break;          // sai da repetição
         }
     }
-    if (numDivisores == 2) {    // se possui apenas 2 divisores, é primo
+    if (num > 1 && !temDivisor) {    // se num > 1 e não possui divisor
         resp.innerText = `${num} é primo.`
     }else{
         resp.innerText = `${num} Não é primo.`
