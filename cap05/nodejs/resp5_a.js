218886d341b99307bce3d711794f79b9f195bb11
Otavio Fernandes da Silva dos Santos <otaviofernandes113@gmail.com>
2026-02-14 09:28:31 -0300

    feat: adiciona arquivo resp5_a

---
+++ cap05/nodejs/resp5_a.js
@@ -0,0 +1,20 @@
+/**
+ * Entrada: Ler o nome e o número de uma fruta
+ * Processamento: repetir a exibição do nome da fruta de acordo com o número informado
+ * Saída: mostrar na tela o número de frutas separando com "*"
+ */
+
+const prompt = require("prompt-sync")();
+
+console.log("\n/****** Programa Repete Fruta ******/ \n");
+console.log("--------------------------------------\n");
+
+const fruta = prompt("Insira uma fruta: ");
+const num = Number(prompt("Nº de frutas: "));
+
+let imprime = "";
+
+for (let i=1; i <= num; i++){
+    console.log(`${fruta}` + " * ")
+}
+
